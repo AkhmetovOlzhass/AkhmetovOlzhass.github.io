3e184521258f071cfbb142a4b602a65cd79ea4bd
sharn1999 <58104696+sharn1999@users.noreply.github.com>
2020-08-30 11:59:13 +0300

    Add files via upload

---
+++ aloe_vera/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 899) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+});
