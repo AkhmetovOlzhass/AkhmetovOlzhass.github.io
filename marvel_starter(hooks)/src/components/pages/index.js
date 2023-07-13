b75889b23e08ba19018bc29c24802365c7127b0a
sharn1999 <58104696+sharn1999@users.noreply.github.com>
2023-07-13 23:06:59 +0600

    Delete marvel_starter(hooks) directory

--- marvel_starter(hooks)/src/components/pages/index.js
+++
@@ -1,6 +0,0 @@
-import MainPage from "./MainPage";
-import ComicsPage from "./ComicsPage";
-import Page404 from "./404";
-import SingleComic from "./SingleComicPage";
-
-export {MainPage, ComicsPage, Page404, SingleComic};
