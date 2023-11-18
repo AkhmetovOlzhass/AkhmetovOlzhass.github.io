906efd9745f99a30c2c43145d5ee09e1f205bf94
sharn1999 <58104696+sharn1999@users.noreply.github.com>
2023-11-19 01:10:38 +0600

    Add files via upload

---
+++ webRP/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+    });
+});
