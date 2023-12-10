394e6a36c8b2849dd272c8546440b8ecdeff7db2
sharn1999 <58104696+sharn1999@users.noreply.github.com>
2023-12-11 01:02:39 +0600

    Add files via upload

--- CP/js/slider.js
+++ CP/js/slider.js
@@ -1,6 +1,8 @@
 $('#promo-slider').slick({
     dots: true,
-    arrows: false
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000
 });
 $('.splide-products').slick();
 $('#most-popular-slide').slick();
